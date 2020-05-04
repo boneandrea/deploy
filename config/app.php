ef1b888ad6c4c9fc1877b3b46fa9920d01ffb9c5
Takuya Tanaka <banchou@peixe.biz>
2020-05-04 09:56:20 +0900

    Change db to pgsql

--- config/app.php
+++ config/app.php
@@ -272,9 +272,9 @@ return [
          */
         'default' => [
             'className' => Connection::class,
-            'driver' => Sqlite::class,
+            'driver' => Postgres::class,
             'persistent' => false,
-            'timezone' => 'UTC',
+            'timezone' => 'Asia/Tokyo',
 
             /**
              * For MariaDB/MySQL the internal default changed from utf8 to utf8mb4, aka full utf-8 support, in CakePHP 3.6
